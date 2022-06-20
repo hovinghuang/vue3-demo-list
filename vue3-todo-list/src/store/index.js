20afed9efb348de263b9b0d4917499b85798c92e
hovinghuang <hovinghuang@163.com>
2022-06-20 23:35:33 +0800

    update: 更新VueX

--- vue3-todo-list/src/store/index.js
+++ vue3-todo-list/src/store/index.js
@@ -2,12 +2,25 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    count: 0,
+    msg: 'Hello Vuex'
   },
   getters: {
+    reverseMsg (state) {
+      return state.msg.split('').reverse().join('')
+    }
   },
   mutations: {
+    increate (state, payload) {
+      state.count += payload
+    }
   },
   actions: {
+    increateAsync (context, payload) {
+      setTimeout(() => {
+        context.commit('increate', payload)
+      }, 2000)
+    }
   },
   modules: {
   }
