a93446b44c5bdc894233a41fc26109d1c922d30f
hovinghuang <hovinghuang@163.com>
2022-07-05 23:50:44 +0800

    update: provide & inject

--- vue3-todo-list/src/App.vue
+++ vue3-todo-list/src/App.vue
@@ -1,5 +1,4 @@
-<template>
-  <!-- <router-view/> -->
+<!-- <template>
   <div>
     <button @click="isShowDemo = !isShowDemo">切换隐藏/显示</button>
     <Demo v-if="isShowDemo"></Demo>
@@ -34,4 +33,32 @@ export default {
   margin: 0;
   padding: 0;
 }
+</style> -->
+
+<template>
+	<div class="app">
+		<h3>我是App组件（祖），{{name}}--{{price}}</h3>
+		<Child/>
+	</div>
+</template>
+
+<script>
+	import { reactive, toRefs, provide } from 'vue'
+	import Child from './components/provide&inject/Child.vue'
+	export default {
+		name:'App',
+		components:{ Child },
+		setup(){
+			let car = reactive({ name:'奔驰', price:'40W' })
+			provide('car',car) // 给自己的后代组件传递数据
+			return {...toRefs(car)}
+		}
+	}
+</script>
+
+<style>
+	.app{
+		background-color: gray;
+		padding: 10px;
+	}
 </style>
